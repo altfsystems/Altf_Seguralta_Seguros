0117a9a2364aa85dd287a681306896162d32436d
Gabriel Aceti <48328349+GabrielAceti@users.noreply.github.com>
2020-05-21 16:58:56 -0300

    21-05-2020 16:58
    Altf_Seguralta versão 2020.05.21.00024
    Alterações no cadastro de venda e visão

--- AltfErp/frmLogin.cs
+++ AltfErp/frmLogin.cs
@@ -24,7 +24,7 @@ namespace AltfErp
             mes = vet[1];
             ano = vet[2];
 
-            lblVersao.Text = "Version "+ano + "." + mes + "." + dia + ".00023";
+            lblVersao.Text = "Version "+ano + "." + mes + "." + dia + ".00024";
         }
 
         private void Login()
